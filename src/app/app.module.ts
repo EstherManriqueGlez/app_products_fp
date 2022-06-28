a2c69a9cb1a0f944aa42616e540343575285d594
Esther Manrique <esther_manrique@epam.com>
2022-06-27 20:07:40 -0500

    Added navBar and data-table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './modules/material/material.module';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+
+const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatTooltipModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatGridListModule,
+  MatSelectModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+export class MaterialModule { }
