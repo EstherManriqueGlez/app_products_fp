266e57f005c338abad268c57778b0e7816d3dccf
EstherManrique <esmango0610@gmail.com>
2022-06-28 20:29:05 -0500

    Added table paginator by query param

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/products';
 import { DataService } from 'src/app/services/data.service';
 
@@ -14,18 +15,25 @@ export class DataTableComponent implements OnInit {
   listProducts!: Product[];
   displayedColumns!: string[];
   dataSource!: MatTableDataSource<any>;
+  startingPage: number = 0;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private _dataService: DataService) {}
+  constructor(private _dataService: DataService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this._dataService.getProducts().subscribe((response) => {
       this.listProducts = response;
       this.displayedColumns = ['productName', 'price', 'isAvailable'];
       this.dataSource = new MatTableDataSource(this.listProducts);
-      this.dataSource.paginator = this.paginator;
+      this.dataSource.paginator = this.paginator; 
     });
+    this.route.queryParams.subscribe((params) => {
+      const page = params['page'];
+      if(page > 0) {
+        this.startingPage = page - 1
+      }
+    })
   }
 
   applyFilter(event: Event) {
@@ -34,7 +42,12 @@ export class DataTableComponent implements OnInit {
   }
 
   pageChanged(event: PageEvent) {
-    console.log(event.pageIndex);
-    
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        page: event.pageIndex + 1
+      },
+      // queryParamsHandling: 'merge'
+    })
   }
 }
